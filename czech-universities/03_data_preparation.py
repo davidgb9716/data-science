0ae4c21ce13c96381f4c591bf448a67f8e530ff8
David González <83733181+davidgb9716@users.noreply.github.com>
2024-06-02 00:58:33 -0600

    Update 03_data_preparation.py

--- czech-universities/03_data_preparation.py
+++ czech-universities/03_data_preparation.py
@@ -3,14 +3,12 @@
 #Import libraries
 import numpy as np
 import pandas as pd
+from scipy.stats import anderson, shapiro
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 
 #Import data
 df = pd.read_csv('data.csv')
 print(df)
-
-#Define the column's data types
-dtype_dict = {col: dtype for col, dtype in zip(df.columns, df.dtypes)}
-print(dtype_dict)
 
 #Identify missing values
 missing_values = df.isna() #Using isna() over isnull() is recommended as a best practice.
