8aef055bc6d5c1f5115c9b0720f2a97658af7948
David González <83733181+davidgb9716@users.noreply.github.com>
2024-06-02 00:57:17 -0600

    Update 03_data_preparation.py

--- czech-universities/03_data_preparation.py
+++ czech-universities/03_data_preparation.py
@@ -33,22 +33,54 @@ df = df.drop_duplicates()
 print(df)
 
 #Identify and handle outliers
-numerical_cols_dict = {k: v for k, v in dtype_dict.items() if v != np.dtype('O')}
-numerical_cols_list = list(numerical_cols_dict.keys())
-df_numerical = df[numerical_cols_list]
-print(df_numerical)
-
-def handle_outliers(data):
-	df_cols = data.columns
-	
-	for col in df_cols:
-		q1 = data[col].quantile(0.25)
-		q3 = data[col].quantile(0.75)
+def handle_outliers(df):
+	def replace_outliers(col):
+		non_zero_col = col[col != 0]
+		if non_zero_col.empty:
+			return col
+		q1 = non_zero_col.quantile(0.25)
+		q3 = non_zero_col.quantile(0.75)
 		iqr = q3 - q1
-		outlier_free_df = df[(df[col] >= (q1 - 1.5 * iqr)) & (df[col] <= (q3 + 1.5 * iqr))]
-		data = outlier_free_df
+		lower_bound = q1 - 1.5 * iqr
+		upper_bound = q3 + 1.5 * iqr
+		median = non_zero_col.median()
+		
+		def replace_value(x):
+			if x == 0:
+				return x
+			elif lower_bound <= x <= upper_bound:
+				return x
+			else:
+				return median
+			
+		return col.apply(replace_value)
 	
-	return data
+	# Apply only to numerical columns
+	numerical_cols = df.select_dtypes(include=['number'])
+	outlier_free_df = numerical_cols.apply(replace_outliers, axis=0)
+	
+	# Combine numerical and non-numerical columns
+	non_numerical_cols = df.select_dtypes(exclude=['number'])
+	result_df = pd.concat([outlier_free_df, non_numerical_cols], axis=1)
+	
+	return result_df
 
-df = handle_outliers(df_numerical)
+df = handle_outliers(df)
+
+df = df.dropna()
+df = df.reset_index(drop=True)
+
 print(df)
+
+categorical_columns = df.select_dtypes(include='object').columns
+
+#Handle inconsistent data (standardize data formats)
+for col in categorical_columns:
+	df[col] = df[col].str.lower()
+
+#Remove Czech columns
+df = df.drop(labels=['college_cs', 'faculty_cs', 'study_program_cs'], axis=1)
+print(df)
+
+#Save Processed DataFrame
+df.to_csv(path_or_buf='processed_dataset.csv', sep=',')
