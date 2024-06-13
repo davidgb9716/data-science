106354b39dee3f4651c75258e8966808ea6feb41
David González <83733181+davidgb9716@users.noreply.github.com>
2024-06-13 17:18:52 -0600

    Update 02_data_understanding.py

--- czech-universities/02_data_understanding.py
+++ czech-universities/02_data_understanding.py
@@ -7,3 +7,7 @@ import pandas as pd
 #Import data
 df = pd.read_csv('data.csv')
 print(df)
+
+#Identify categorical and numerical columns in our DataFrame
+df_categorical = df.select_dtypes(include='object')
+df_numerical = df.select_dtypes(include='number')
